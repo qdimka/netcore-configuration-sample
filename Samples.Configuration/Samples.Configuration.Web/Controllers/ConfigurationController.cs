542c2318f9cb1f97e79c90af8f2f834c9a2dfc48
Дмитрий <qdimka1991@mail.ru>
2019-01-10 10:14:43 +0300

    Тестовый сервис

---
+++ Samples.Configuration/Samples.Configuration.Web/Controllers/ConfigurationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Samples.Configuration.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+    }
+}
--- Samples.Configuration/Samples.Configuration.WithAppSettings/Startup.cs
+++ Samples.Configuration/Samples.Configuration.WithAppSettings/Startup.cs
@@ -25,6 +25,7 @@ namespace Samples.Configuration.WithAppSettings
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var myCustomSetting = Configuration["MyCustomSettings:Name"];
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
