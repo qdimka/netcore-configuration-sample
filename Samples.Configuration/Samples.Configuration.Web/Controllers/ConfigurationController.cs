79899e22e57b9d478db740fddbcfb19a19a3f82b
Дмитрий <qdimka1991@mail.ru>
2019-01-10 15:14:13 +0300

    Тестовый сервер

--- Samples.Configuration/Samples.Configuration.Web/Controllers/ConfigurationController.cs
+++ Samples.Configuration/Samples.Configuration.Web/Controllers/ConfigurationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Samples.Configuration.Web.Controllers
 {
@@ -6,5 +7,41 @@ namespace Samples.Configuration.Web.Controllers
     [ApiController]
     public class ConfigurationController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("file")]
+        public IActionResult GetFromConfigFile()
+        {
+            return Ok(new
+            {
+                name = "SettingsFile:Key",
+                key = _configuration["SettingsFile:Key"]
+            });
+        }
+
+        [HttpGet("secret")]
+        public IActionResult GetFromSecret()
+        {
+            return Ok(new
+            {
+                name = "SettingsSecret:Key",
+                key = _configuration["SettingsSecret:Key"]
+            });
+        }
+
+        [HttpGet("environment")]
+        public IActionResult GetFromEnvironment()
+        {
+            return Ok(new
+            {
+                name = "SettingsEnvironment:Key",
+                key = _configuration["SettingsEnvironment:Key"]
+            });
+        }
     }
 }
