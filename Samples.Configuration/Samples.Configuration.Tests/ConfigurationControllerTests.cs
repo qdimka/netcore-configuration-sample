fa30fe837757dc6934ab7faea3377ea49e6ddcb1
Дмитрий <qdimka1991@mail.ru>
2019-01-12 19:23:28 +0300

    Добавил интеграционный тест

---
+++ Samples.Configuration/Samples.Configuration.Tests/ConfigurationControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Samples.Configuration.Web;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Samples.Configuration.Tests
+{
+    public class ConfigurationControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly HttpClient _client;
+
+        public ConfigurationControllerTests(ITestOutputHelper testOutputHelper, WebApplicationFactory<Startup> factory)
+        {
+            _testOutputHelper = testOutputHelper;
+            _client = factory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("api/configuration/file", "I am appsettings.Development")]
+        [InlineData("api/configuration/secret", "I am Secret")]
+        [InlineData("api/configuration/environment", "I am Environment")]
+        [InlineData("api/configuration/yaml", "I am Yaml")]
+        public async Task Configuration_Should_Contains_Expected_Value(string url, string expectedValue)
+        {
+            var result = await _client.GetAsync(url);
+            var content = await result.Content.ReadAsStringAsync();
+
+            _testOutputHelper.WriteLine($"\"{content}\" contains \"{expectedValue}\"");
+
+            Assert.True(result.IsSuccessStatusCode);
+            Assert.NotNull(content);
+            Assert.Contains(expectedValue, content);
+        }
+    }
+}
--- Samples.Configuration/Samples.Configuration.Tests/obj/Debug/netcoreapp2.2/Samples.Configuration.Tests.AssemblyInfo.cs
+++ Samples.Configuration/Samples.Configuration.Tests/obj/Debug/netcoreapp2.2/Samples.Configuration.Tests.AssemblyInfo.cs
@@ -11,6 +11,8 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Samples.Configuration.Web, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\qdimk\\source\\repos\\netcore-configuration-sample\\Samples.Configuration\\Sa" +
+    "mples.Configuration.Web", "Samples.Configuration.Web.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Samples.Configuration.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
